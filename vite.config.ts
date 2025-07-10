77305edb7901d61f456e451b810a0da61c65d30c
WallaceBarrosDev <w.wallacebarro.cruz@gmail.com>
2025-07-09 22:39:11 -0300

    config

--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,6 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [vue()],
+  base: '/estimativa-de-lucro/', // 👈 isso aqui é ESSENCIAL
+  plugins: [vue()]
 })
