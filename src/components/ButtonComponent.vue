cbacbd7ef17c44a19942fe8e2ac13791fbe5b2f2
Kayo Alves <kayosilva577@gmail.com>
2025-07-12 21:30:27 -0300

    feat: add button component

---
+++ src/components/ButtonComponent.vue
@@ -0,0 +1,11 @@
+<template>
+    <button>Button</button> 
+</template>
+
+<style scoped>
+    button {
+        background-color: #84FF6C;
+        color: black; 
+        font-size: 1.2rem;
+    }
+</style>
