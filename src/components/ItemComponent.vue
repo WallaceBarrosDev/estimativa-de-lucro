b9c501a154624a1c6f7c8ef49d89629b8f11b595
WallaceBarrosDev <w.wallacebarro.cruz@gmail.com>
2025-07-20 20:44:27 -0300

    feat: sistema de slot

---
+++ src/components/ItemComponent.vue
@@ -0,0 +1,15 @@
+<template>
+    <li>
+        <slot/>
+        <p v-if="slotVoid">Meu Item</p>    
+    </li>
+</template>
+
+<script lang="ts" setup>
+import { useSlots } from 'vue';
+
+const slot = useSlots()
+const slotVoid = !slot.default || slot.default().length === 0
+
+
+</script>
