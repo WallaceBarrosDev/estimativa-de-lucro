25a18ea50932ebf324d8670842362b88f09ba91b
WallaceBarrosDev <w.wallacebarro.cruz@gmail.com>
2025-07-30 12:46:32 -0300

    feature: ListComponent

---
+++ src/components/ListComponent.vue
@@ -0,0 +1,17 @@
+<template>
+    <ul>
+        <ItemComponent v-for="({price, weight, amount, name}, index) in products" :key="index"
+            :price="price" :weight="weight" :amount="amount"> {{ name }}
+        </ItemComponent>
+    </ul>
+</template>
+
+<script lang="ts" setup>
+import type { Product } from '../types';
+import ItemComponent from './ItemComponent.vue';
+
+const { products } = defineProps<{products:Array<Product>}>()
+</script>
+
+<style scoped>
+</style>
